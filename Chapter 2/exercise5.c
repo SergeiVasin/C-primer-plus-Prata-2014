ca103f6ea8f42133fbf576c38fc7b0fa8bc34200
SergeiVasin <101241450+SergeiVasin@users.noreply.github.com>
2022-03-09 10:49:11 +0300

    Create exercise5.c

---
+++ Chapter 2/exercise5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void br();
+void ic();
+int main(void)  {
+br();
+printf(", ");
+ic();
+printf(" .\n");
+ic();
+printf(".\n");
+br();
+printf(".\n");
+return 0;   }
+void br()   {
+printf("Бразилия, Россия");   }
+void ic()   {
+printf("Индия, Китай");     }

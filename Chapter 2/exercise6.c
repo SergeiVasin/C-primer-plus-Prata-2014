f55ba94f4d0329f6d37764a9f631ca7f6fb49011
SergeiVasin <101241450+SergeiVasin@users.noreply.github.com>
2022-03-09 10:57:06 +0300

    Create exercise6.c

---
+++ Chapter 2/exercise6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)  {
+int toes=10;
+int toes_tw;
+int toes_sq;
+toes_tw=2*toes;
+toes_sq=toes*toes;
+printf("Всего: %d\n", toes);
+printf("Всего на двух: %d\n", toes_tw);
+printf("Всего в квадрате: %d\n", toes_sq);
+return 0;   }
